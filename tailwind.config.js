0be5a3a45a72c2f7b73c575cfeedaf53ee77fea2
Dennis Miceli <dennis.miceli@hotmail.ch>
2022-05-27 14:40:51 +0200

    Add Navbar and layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+const fontFamily = defaultTheme.fontFamily;
+fontFamily['sans'] = [
+  'Roboto', // <-- Roboto is a default sans font now
+  'system-ui',
+  // <-- you may provide more font fallbacks here
+];
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js"
   ],  theme: {
+    fontFamily: fontFamily, // <-- this is where the override is happening
     extend: {},
   },
-  plugins: [],
+  plugins: [
+  ],
 }
