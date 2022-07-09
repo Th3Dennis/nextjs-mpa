5d9a6ad0a32fcc8c7001e43e7b25c387120f9f77
Th3Dennis <dennis.miceli@accenture.com>
2022-07-09 18:08:22 +0200

    Add bootstrap js

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,17 @@
 import Layout from "../components/_layout";
 import '../styles/bootstrap.css'
+import Script from "next/script";
 
 
 
 function MyApp({ Component, pageProps }) {
   return (
+    <>
+    <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"/>
     <Layout>
       <Component {...pageProps} />
     </Layout>
+    </>
   );
 }
 
--- pages/copyright.js
+++ pages/copyright.js
@@ -1,7 +1,56 @@
-import React from 'react'
+import React from "react";
 
 export default function copyright() {
   return (
-    <div>copyright</div>
-  )
+    <div className="container border border-2 mt-2 rounded">
+      <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
+        <li className="nav-item" role="presentation">
+          <button
+            className="nav-link active"
+            id="pills-copyright-tab"
+            data-bs-toggle="pill"
+            data-bs-target="#pills-copyright"
+            type="button"
+            role="tab"
+            aria-controls="pills-copyright"
+            aria-selected="true"
+          >
+            Copyright
+          </button>
+        </li>
+        <li className="nav-item" role="presentation">
+          <button
+            className="nav-link"
+            id="pills-licenses-tab"
+            data-bs-toggle="pill"
+            data-bs-target="#pills-licenses"
+            type="button"
+            role="tab"
+            aria-controls="pills-licenses"
+            aria-selected="false"
+          >
+            Profile
+          </button>
+        </li>
+      </ul>
+      <div className="tab-content" id="pills-tabContent">
+        <div
+          className="tab-pane fade show active"
+          id="pills-copyright"
+          role="tabpanel"
+          aria-labelledby="pills-home-tab"
+        >
+            <h3>Copyright</h3>
+        </div>
+        <div
+          className="tab-pane fade"
+          id="pills-licenses"
+          role="tabpanel"
+          aria-labelledby="pills-profile-tab"
+        >
+            <h3>Licenses</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
