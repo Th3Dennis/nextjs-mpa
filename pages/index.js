8290db86b6d79a3c1da70698de68e74e0a15168e
Th3Dennis <dennis.miceli@accenture.com>
2022-07-11 08:57:24 +0200

    Add scroll animation to index

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,46 @@
+import {
+  Animator,
+  batch,
+  FadeIn,
+  ScrollContainer,
+  ScrollPage,
+  StickyIn,
+  ZoomIn,
+  Sticky,
+  Fade,
+  MoveOut,
+  Move,
+} from "react-scroll-motion";
+
 export default function Home() {
+  const zoomInScrollOut = batch(StickyIn(), FadeIn(), ZoomIn());
+  const fadeUp = batch(Fade(), Sticky(), Move());
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col">
-          <h2>Purpose</h2>
-          <p>The purpose of this website is to show all the possibilities that can be created using NextJS.</p>
-          <p>On the following pages can examples be seen with animations and a well thought out color scheme.</p>
-        </div>
-      </div>
-      
+    <div className=".bg-primary">
+      <ScrollContainer>
+        <ScrollPage>
+          <Animator animation={batch(Sticky(), Fade(), MoveOut(0, -200))}>
+            <h2>What&apos;s the porpuse of this page?</h2>
+          </Animator>
+        </ScrollPage>
+
+        <ScrollPage>
+          <Animator animation={zoomInScrollOut}>
+            <h2>
+              The purpose of this website is to show all the possibilities that
+              can be created using NextJS.
+            </h2>
+          </Animator>
+        </ScrollPage>
+
+        <ScrollPage>
+          <Animator animation={fadeUp}>
+            <h2>
+              Go and explore the next pages by clicking the links in the Navbar
+            </h2>
+          </Animator>
+        </ScrollPage>
+      </ScrollContainer>
     </div>
   );
 }
