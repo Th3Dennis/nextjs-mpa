6e1ac17b4238c05f14efda0a12414db6c2ef05f6
Th3Dennis <dennis.miceli@accenture.com>
2022-07-11 10:40:12 +0200

    Change to h1

--- pages/index.js
+++ pages/index.js
@@ -20,24 +20,24 @@ export default function Home() {
       <ScrollContainer>
         <ScrollPage>
           <Animator animation={batch(Sticky(), Fade(), MoveOut(0, -200))}>
-            <h2>What&apos;s the porpuse of this page?</h2>
+            <h1>What&apos;s the porpuse of this page?</h1>
           </Animator>
         </ScrollPage>
 
         <ScrollPage>
           <Animator animation={zoomInScrollOut}>
-            <h2>
+            <h1>
               The purpose of this website is to show all the possibilities that
               can be created using NextJS.
-            </h2>
+            </h1>
           </Animator>
         </ScrollPage>
 
         <ScrollPage>
           <Animator animation={fadeUp}>
-            <h2>
+            <h1>
               Go and explore the next pages by clicking the links in the Navbar
-            </h2>
+            </h1>
           </Animator>
         </ScrollPage>
       </ScrollContainer>
