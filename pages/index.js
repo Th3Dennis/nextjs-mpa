fe50a1939dc0dad2672b8e53decc3736c6a4b391
Th3Dennis <dennis.miceli@accenture.com>
2022-07-11 08:36:12 +0200

    Add purpose of page to index

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
 export default function Home() {
   return (
-    <div>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h2>Purpose</h2>
+          <p>The purpose of this website is to show all the possibilities that can be created using NextJS.</p>
+          <p>On the following pages can examples be seen with animations and a well thought out color scheme.</p>
+        </div>
+      </div>
+      
     </div>
   );
 }
